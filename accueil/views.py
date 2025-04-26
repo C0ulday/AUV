81664a32dcb4598a92e6c20a9cb6e766919109bb
C0ulday <mamasitaurslut@gmail.com>
2025-04-26 09:01:34 +0200

    abandon merging

--- accueil/views.py
+++ accueil/views.py
@@ -7,19 +7,3 @@ def index(request):
 
 def connexion(request):
     return render(request,'monespace.html',context={})
-
-def connexionEspace(request):
-    context = {}
-    if request.method == "POST":
-        identifiant = request.POST.get('identifiant')
-        password = request.POST.get('password')
-
-        user = Member.objects.filter(email=identifiant, password=password).first()
-
-        if user:
-            request.session['user_id'] = user.id
-           # return redirect('intranet_home')  # redirige vers l'app intranet
-        else:
-            context['error'] = "Identifiant ou mot de passe incorrect"
-
-    return render(request, 'accueil/monespace.html', context)
--- siteAUV/urls.py
+++ siteAUV/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
     path('connexion/', connexion, name='connexion'),
-    path('connexionEspace/', connexionEspace, name='connexionEspace'),
     ]
